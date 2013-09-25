6e424dae972f7327aedc218901e901dd466f6952
IsaacNeal <inp0420@gmail>
2013-09-25 15:35:10 -0700

    Update SecTools.php

--- SecTools.php
+++ SecTools.php
@@ -1,15 +1,4 @@
 <?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * Description of SecTools
- *
- * @author Isaac
- */
 class SecTools {
     
     public static function sanitizeUserFields($value) {
